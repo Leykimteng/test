d0048edffed44c6bc76e67289ed61656f344bad1
Kim Teng <leykimteng@gmail.com>
2018-06-14 13:43:47 +0700

    Fibonanci

---
+++ fibonanci.scala
@@ -0,0 +1,16 @@
+object fibonanci
+{
+  def main(args: Array[String]): Unit = {
+    println("Enter The Number: ")
+    var a=scala.io.StdIn.readInt()
+    var b:Int=0 ; var c:Int= 1;var t:Int=0;
+    for(i<-1 to a)
+    {
+      println(b)
+      b=b+c
+      t= b
+      b=c
+      c=t
+    }
+  }
+}
