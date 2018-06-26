4524fcededa9752959be0095ccd25f1a76b06a4c
Kim Teng <leykimteng@gmail.com>
2018-06-26 13:04:50 +0700

    i forget to commit this

---
+++ List1.scala
@@ -0,0 +1,9 @@
+object list
+{
+  def main(args: Array[String]): Unit = {
+    var fruits = List ("Bannna","Apple","Coconut")
+    var t="Kiwi" :: fruits
+    fruits = ("Strawberry") :: fruits
+    println(fruits)
+  }
+}
