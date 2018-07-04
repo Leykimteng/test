5aafe33b76bf10dc41d2fd7ea717cc825deee014
Kim Teng <leykimteng@gmail.com>
2018-07-04 10:13:12 +0700

    Change in sum

--- SumARR.scala
+++ SumARR.scala
@@ -3,13 +3,36 @@ object sum
   def main(args: Array[String]): Unit = {
     println("Enter the Number of array")
     var i= scala.io.StdIn.readInt()
-    var sum:Int=0
-    var arr= new Array[Int](5)
+    var sum,max:Int=0
+    var arr1,arr2= new Array[Int](i)
+    var arr = new Array[Int](2*i)
     for(j<-0 to i-1) {
       println("Enter the number for "+(j+1)+" Element")
-      arr(j) = scala.io.StdIn.readInt()
+      arr1(j) = scala.io.StdIn.readInt()
       sum = sum + arr(j)
     }
-      print("Sum of "+i+" numer is : "+sum)
+    for(j<-0 to i-2)
+        if(arr1(j)<arr1(j+1))
+          max=arr1(j+1)
+      println("Sum of "+i+" numer is : "+sum)
+      println("Max Number is : "+max)
+    max=0
+    println("Enter second array of "+i+" number : ")
+    for(j<-0 to i-1) {
+      println("Enter the number for " + (j + 1) + " Element")
+      arr2(j) = scala.io.StdIn.readInt()
+    }
+    for(j<-0 to 2*(i-1))
+      {
+        if(j<i)
+          arr(j)=arr1(j)
+        else {
+          arr(j) = arr2(max)
+          max += 1
+        }
+      }
+
+    for(j<-0 to 2*(i-1))
+      print(arr(j)+ " ")
   }
 }
